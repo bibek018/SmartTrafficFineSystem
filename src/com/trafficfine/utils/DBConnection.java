cfe68ad648f39ba2ec823e969c7c3d561c743d76
Bibek Ojha <ojhabibek505@gmail.com>
2026-05-14 22:39:41 +0530

    updated code

--- src/com/trafficfine/utils/DBConnection.java
+++ src/com/trafficfine/utils/DBConnection.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class DBConnection {
     private static final String URL      = "jdbc:mysql://localhost:3306/traffic_fine_db?useSSL=false&serverTimezone=UTC";
     private static final String USER     = "root";
-    private static final String PASSWORD = "12345678"; // ← change to your MySQL password
+    private static final String PASSWORD = "root"; // ← change to your MySQL password
 
     private static Connection connection = null;
 
